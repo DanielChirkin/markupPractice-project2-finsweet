b8043d40e9419f24ee829f0f7975b62abe486ab6
Daniel <nickobalick22@gmail.com>
2022-07-09 21:22:30 +0900

    installed and adjusted webpack

---
+++ webpack.config.js
@@ -0,0 +1,69 @@
+const path = require('path')
+const HTMLWebpackPlugin = require('html-webpack-plugin')
+const { CleanWebpackPlugin } = require('clean-webpack-plugin')
+const MiniCssExtractPlugin = require("mini-css-extract-plugin")
+const CssMinimizerPlugin = require("css-minimizer-webpack-plugin")
+
+const isDev = process.env.NODE_ENV === 'development'
+console.log('IS DEV:', isDev, "-", process.env.NODE_ENV);
+
+const filename = ext => isDev ? `[name].${ext}` : `[name].[contenthash].${ext}`
+
+module.exports = {
+  context: path.resolve(__dirname, 'src'),
+  entry: './js/index.js',
+  output: {
+    filename: filename('js'),
+    path: path.resolve(__dirname, 'dist')
+  },
+  resolve: {
+    alias: {
+      '@': path.resolve(__dirname, 'src')
+    }
+  },
+  plugins: [
+    new HTMLWebpackPlugin({
+      template: './index.html'
+    }),
+    new CleanWebpackPlugin(),
+    new MiniCssExtractPlugin({
+      filename: filename('css')
+    })
+  ],
+  module: {
+    rules: [
+      {
+        test: /\.css$/,
+        use: [
+          MiniCssExtractPlugin.loader,
+          'css-loader'
+        ]
+      },
+      {
+        test: /\.s[ac]ss$/i,
+        use: [
+          MiniCssExtractPlugin.loader,
+          'css-loader',
+          'sass-loader'
+        ]
+      },
+      {
+        test: /\.(png|svg|jpg|gif)$/,
+        type: 'asset/resource'
+      },
+      {
+        test: /\.html$/i,
+        loader: "html-loader",
+      }
+    ]
+  },
+  optimization: {
+    splitChunks: {
+      chunks: 'all'
+    },
+    minimizer: [
+      `...`,
+      new CssMinimizerPlugin(),
+    ]
+  }
+}
